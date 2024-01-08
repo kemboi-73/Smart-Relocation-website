968dad2d04a1295d8711862ea49347dd1cd1f6b2
Mikey_254 <123319720+kemboi-73@users.noreply.github.com>
2024-01-08 20:56:12 +0300

    js update

--- js/script.js
+++ js/script.js
@@ -1,32 +1,64 @@
-// Select The Elements
-var toggle_btn;
-var big_wrapper;
-var hamburger_menu;
+// // Select The Elements
+// var toggle_btn;
+// var big_wrapper;
+// var hamburger_menu;
 
-function declare() {
-  toggle_btn = document.querySelector(".hamburger-menu");
-  big_wrapper = document.querySelector(".big-wrapper");
-  hamburger_menu = document.querySelector(".hamburger-menu");
+// function declare() {
+//   toggle_btn = document.querySelector(".hamburger-menu");
+//   big_wrapper = document.querySelector(".big-wrapper");
+//   hamburger_menu = document.querySelector(".hamburger-menu");
 
-}
+// }
 
-const main = document.querySelector("main");
+// const main = document.querySelector("main");
 
-declare();
+// declare();
 
-let dark = false;
+// let dark = false;
 
+// function toggleAnimation() {
+//   // Clone the wrapper
+//   dark = !dark;
+//   let clone = big_wrapper.cloneNode(true);
+//   if (dark) {
+//     clone.classList.remove("light");
+//     clone.classList.add("dark");
+//   } else {
+//     clone.classList.remove("dark");
+//     clone.classList.add("light");
+//   }
+//   clone.classList.add("copy");
+//   main.appendChild(clone);
+
+//   document.body.classList.add("stop-scrolling");
+
+//   clone.addEventListener("animationend", () => {
+//     document.body.classList.remove("stop-scrolling");
+//     big_wrapper.remove();
+//     clone.classList.remove("copy");
+//     // Reset Variables
+//     declare();
+//     events();
+//   });
+// }
+
+// function toggleHamburgerMenu() {
+//   big_wrapper.classList.toggle("active");
+// }
+
+// function events() {
+//   toggle_btn.addEventListener("click", toggleAnimation);
+//   hamburger_menu.addEventListener("click", toggleHamburgerMenu);
+// }
+
+// events();
 function toggleAnimation() {
   // Clone the wrapper
-  dark = !dark;
   let clone = big_wrapper.cloneNode(true);
-  if (dark) {
-    clone.classList.remove("light");
-    clone.classList.add("dark");
-  } else {
-    clone.classList.remove("dark");
-    clone.classList.add("light");
-  }
+
+  // Toggle the active class for the hamburger menu
+  clone.classList.toggle("active");
+
   clone.classList.add("copy");
   main.appendChild(clone);
 
@@ -41,14 +73,3 @@ function toggleAnimation() {
     events();
   });
 }
-
-function toggleHamburgerMenu() {
-  big_wrapper.classList.toggle("active");
-}
-
-function events() {
-  toggle_btn.addEventListener("click", toggleAnimation);
-  hamburger_menu.addEventListener("click", toggleHamburgerMenu);
-}
-
-events();
